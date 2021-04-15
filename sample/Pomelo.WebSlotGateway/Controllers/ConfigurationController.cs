2807847ff1770d5d1ab5f4077f8b01e5d7c6353c
Yuko Zheng <yuko.zheng@autodesk.com>
2021-04-15 09:02:27 +0800

    Enhancement

--- sample/Pomelo.WebSlotGateway/Controllers/ConfigurationController.cs
+++ sample/Pomelo.WebSlotGateway/Controllers/ConfigurationController.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Pomelo.Net.Gateway.EndpointManager;
 using Pomelo.WebSlotGateway.Models;
+using Pomelo.WebSlotGateway.Utils;
 
 namespace Pomelo.WebSlotGateway.Controllers
 {
@@ -27,21 +30,53 @@ namespace Pomelo.WebSlotGateway.Controllers
         [HttpPatch]
         public async ValueTask<IEnumerable<Config>> Put(
             [FromBody] SetConfigurationRequestViewModel request,
+            [FromServices] TcpEndpointManager tcpEndpointManager,
+            [FromServices] ConfigurationHelper config,
             [FromServices] SlotContext db,
             CancellationToken cancellationToken = default)
         {
-            var slots = await db.Configurations
+            var configurations = await db.Configurations
                 .ToListAsync(cancellationToken);
 
-            foreach (var slot in slots)
+            var needReset = false;
+            foreach (var configuration in configurations)
             {
-                var update = request.Configurations.SingleOrDefault(x => x.Key == slot.Key);
+                var update = request.Configurations.SingleOrDefault(x => x.Key == configuration.Key);
                 if (update != null)
                 {
-                    slot.Value = update.Value;
+                    if (configuration.Key == ConfigurationHelper.KeyLocalEndpoint && configuration.Value != update.Value)
+                    {
+                        needReset = true;
+                    }
+                    configuration.Value = update.Value;
                 }
             }
             await db.SaveChangesAsync(cancellationToken);
+
+            if (needReset) 
+            {
+                // Remove all rules
+                var slots = await db.Slots.ToListAsync(cancellationToken);
+                db.RemoveRange(slots);
+                await db.SaveChangesAsync(cancellationToken);
+                foreach (var slot in slots)
+                {
+                    await tcpEndpointManager.RemoveAllRulesFromUserIdentifierAsync(slot.Id.ToString(), cancellationToken);
+                    await tcpEndpointManager.RemovePreCreateEndpointRuleAsync(slot.Id.ToString(), cancellationToken);
+                }
+
+                // Setup rules
+                foreach (var rule in slots)
+                {
+                    await tcpEndpointManager.InsertPreCreateEndpointRuleAsync(
+                        rule.Id.ToString(),
+                        await config.GetLocalEndpointAsync(),
+                        await AddressHelper.ParseAddressAsync(rule.Destination, 0),
+                        Startup.RouterId,
+                        Startup.TunnelId);
+                }
+                await tcpEndpointManager.EnsurePreCreateEndpointsAsync();
+            }
 
             return await db.Configurations
                 .ToListAsync(cancellationToken);

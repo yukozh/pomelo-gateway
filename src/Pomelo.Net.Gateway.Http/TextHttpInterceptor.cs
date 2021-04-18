f8b7641055c0f362dfd5bed38e903d07b76bd33d
Yuko Zheng <hi@yuko.me>
2021-04-18 23:18:03 +0800

    Update TextHttpInterceptor.cs

--- src/Pomelo.Net.Gateway.Http/TextHttpInterceptor.cs
+++ src/Pomelo.Net.Gateway.Http/TextHttpInterceptor.cs
@@ -17,9 +17,12 @@ namespace Pomelo.Net.Gateway.Http
             body = body.Replace("Site", "Intercepted");
             var bytes = Encoding.UTF8.GetBytes(body);
             context.Response.Headers.AddOrUpdate("content-length", bytes.Length.ToString());
-            await base.BackwardResponseHeaderAsync(context, cancellationToken);
+            await BackwardResponseHeaderAsync(context, cancellationToken);
             await context.Response.DestinationStream.WriteAsync(bytes, 0, bytes.Length);
             return true;
         }
+
+        public override async ValueTask<bool> ForwardRequestAsync(HttpTunnelContext context, CancellationToken cancellationToken = default)
+            => false;
     }
 }

3fbc2e4b755045f3ee01321704d2c4b429582878
Yuko Zheng <hi@yuko.me>
2021-04-20 19:41:26 +0800

    Update GoogleInterceptor.cs

--- sample/Pomelo.WebSlotGateway/Utils/GoogleInterceptor.cs
+++ sample/Pomelo.WebSlotGateway/Utils/GoogleInterceptor.cs
@@ -17,7 +17,8 @@ namespace Pomelo.WebSlotGateway.Utils
             var body = await context.Response.ReadAsStringAsync();
             body = body
                 .Replace("\"https:\"==window.location.protocol", "(\"https:\"==window.location.protocol||\"http:\"==window.location.protocol)")
-                .Replace("Google 搜索的运作方式 </a>", "Google 搜索的运作方式 </a><a class='pHiOh' href='https://github.com/pomelofoundation' target='_blank'>Pomelo Foundation</a>");
+                .Replace("Google 搜索的运作方式 </a>", "Google 搜索的运作方式 </a><a class='pHiOh' href='https://github.com/pomelofoundation' target='_blank'>Pomelo Foundation</a>")
+                .Replace("条款</a>", "条款</a><a class='JWaTvb Fx4vi' href='https://github.com/pomelofoundation' target='_blank'>Pomelo Foundation</a>");
             await context.Response.WriteTextAsync(
                 body,
                 "text/html",
